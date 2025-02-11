4a789e8ee5b6534507f2c84db8d14004bddc4f4d
Kamphaengphet <kamphaengphet.s@kkumail.com>
2025-02-12 06:06:33 +0700

    Update: CallPublicationController.php

--- SourceCodeVersion_1/app/Http/Controllers/CallPublicationController.php
+++ SourceCodeVersion_1/app/Http/Controllers/CallPublicationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Services\APIFetcher\GoogleScholarAPIService;
 use App\Services\APIFetcher\ScopusAPIService;
+use App\Services\APIFetcher\TciAPIService;
 use App\Services\APIFetcher\WosAPIService;
 use App\Models\User;
 use App\Services\WebScraper\IDScraper;
@@ -21,20 +22,30 @@ class CallPublicationController extends Controller
         $userId = Crypt::decrypt($userId);
         $user = User::find($userId);
         $lName_fName = $user->lname_en . ' ' . $user->fname_en;
-//        $wosAPI = new WosAPIService('4e58ee08d1f6ba5b493b7dc227cc59d21c84e8f3');
-//        $wosPublications = $wosAPI->getResearcherPublications($lName_fName);
-//        $wosAPI->saveWOSPublications($wosPublications, $userId);
+        $fName_lName = $user->fname_en . ' ' . $user->lname_en;
 
+        /* WOS API */
+        $wosAPI = new WosAPIService('4e58ee08d1f6ba5b493b7dc227cc59d21c84e8f3');
+        $wosPublications = $wosAPI->getResearcherPublications($lName_fName);
+        $wosAPI->saveWOSPublications($wosPublications, $userId);
+
+        /* Scopus API */
         $scopusAPI = new ScopusAPIService();
         $scopusPublications = $scopusAPI->fetchData($userId);
         $scopusAPI->saveScopusPublications($scopusPublications, $userId);
 
-//        $idScraper = new IDScraper(new Client());
-//        $authorNames = [$lName_fName];
-//        $authorScholarId = $idScraper->search($authorNames);
-//        $googleScholarAPI = new GoogleScholarAPIService('6b2865ac4c28b16a9e0b76c9306d8ff0689620635b9923c5d90e63609218dc26');
-//        $googleScholarPublications = $googleScholarAPI->getResearcherPublications($authorScholarId[0]);
-//        $googleScholarAPI->saveGoogleScholarPublications($googleScholarPublications, $userId);
+        /* TCI web scraper */
+        $tciPublication = TciAPIService::extractRelevantData($fName_lName);
+        TciAPIService::saveTciPublications($tciPublication, $userId);
+
+
+        /* Google Scholar API and web scraper */
+        $idScraper = new IDScraper(new Client());
+        $authorNames = [$fName_lName];
+        $authorScholarId = $idScraper->search($authorNames);
+        $googleScholarAPI = new GoogleScholarAPIService('6b2865ac4c28b16a9e0b76c9306d8ff0689620635b9923c5d90e63609218dc26');
+        $googleScholarPublications = $googleScholarAPI->getResearcherPublications($authorScholarId[0]);
+        $googleScholarAPI->saveGoogleScholarPublications($googleScholarPublications, $userId);
 
         return redirect()->back();
     }
