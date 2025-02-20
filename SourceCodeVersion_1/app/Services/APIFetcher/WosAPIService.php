612dc5efddaf25dfd734eef24d6eec3825daf060
Kamphaengphet <kamphaengphet.s@kkumail.com>
2025-02-20 18:49:12 +0700

    Update: WosAPIService.php

--- SourceCodeVersion_1/app/Services/APIFetcher/WosAPIService.php
+++ SourceCodeVersion_1/app/Services/APIFetcher/WosAPIService.php
@@ -42,44 +42,6 @@ class WosAPIService
         }
     }
 
-    public function extractPuclicationsData($jsonData)
-    {
-        $data = json_decode($jsonData, true);
-        if (!isset($data['hits']) || !is_array($data['hits'])) {
-            die("Invalid JSON structure");
-        }
-
-        $result = [];
-
-        foreach ($data['hits'] as $entry) {
-            $result[] = [
-                'paper_name' => $entry['title'] ?? null,
-                'abstract' => null,
-                'paper_type' => $entry['types'][0] ?? null,
-                'paper_subtype' => $entry['sourceTypes'][0] ?? null,
-                'paper_sourcetitle' => $entry['source']['sourceTitle'] ?? null,
-                'keyword' => isset($entry['keywords']['authorKeywords']) ?
-                            implode(", ",
-                                $entry['keywords']['authorKeywords']
-                            ) : null,
-                'paper_url' => $entry['links']['record'] ?? null,
-                'publication' => $entry['source']['sourceTitle'] ?? null,
-                'paper_yearpub' => $entry['source']['publishYear'] ?? null,
-                'paper_volume' => $entry['source']['volume'] ?? null,
-                'paper_issue' => $entry['source']['issue'] ?? null,
-                'paper_citation' => $entry['citations'][0]['count'] ?? 0,
-                'paper_page' => $entry['source']['pages']['range'] ?? null,
-                'paper_doi' => $entry['identifiers']['doi'] ?? null,
-                'paper_funder' => $entry['names']['sponsors'] ??null,
-                'reference_number' => null,
-                'created_at' => null,
-                'updated_at' => null
-            ];
-        }
-
-        return $result;
-    }
-
     public function saveWOSPublications(array $papers, string $userId): void
     {
         foreach ($papers['hits'] as $paper) {
