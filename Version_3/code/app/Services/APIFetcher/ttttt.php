d95244c63d9cadb919b826f0604f7ad55eebab42
Kamphaengphet <kamphaengphet.s@kkumail.com>
2025-03-09 23:09:46 +0700

    Add: implement extractDataToObject method for processing paper data and integrate Google Scholar API search functionality

--- Version_3/code/app/Services/APIFetcher/ttttt.php
+++ Version_3/code/app/Services/APIFetcher/ttttt.php
@@ -1,6 +1,17 @@
 <?php
-$li = ['a', 'b', 'c'];
+namespace App\Services\APIFetcher;
 
-foreach ($li as $index => $value) {
-    echo $index;
-}
+require "vendor/autoload.php";
+
+use GoogleSearchResults;
+
+$search = new GoogleSearchResults('6b2865ac4c28b16a9e0b76c9306d8ff0689620635b9923c5d90e63609218dc26');
+$query = [
+    "engine" => "google_scholar_author",
+    "author_id" => "00JXDiUAAAAJ",
+];
+
+$result = $search->get_json($query);
+
+print_r(json_encode($result, JSON_PRETTY_PRINT));
+
